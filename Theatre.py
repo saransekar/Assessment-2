e47c504c1febc45a51e3643706b4833130660e26
[saransekar] <[saran@5gindia.net]>
2019-04-15 19:37:49 -0300

    Completed theatre.py for second assessment-2

---
+++ Theatre.py
@@ -0,0 +1,64 @@
+class TheatreA:
+    def __init__(self, theatreName, movieName, showTime, seat, price):
+        self.__theatre = theatreName
+        self.__movieName = movieName
+        self.__showTime = showTime
+        self.__seat = seat
+        self.__price = price
+
+    def setTheatreName(self, Theatre):
+        self.__theatre = Theatre        
+
+    def getTheatreName(self):
+        return self.__theatre    
+
+    def getMovieName(self):            
+        return self.__movieName
+
+    def getShowTime(self):
+        return self.__showTime    
+
+    def setSeat(self, Seat):
+        self.__seat = Seat     
+
+    def getSeat(self):
+        return self.__seat      
+
+    def getPrice(self):
+        return self.__price      
+
+    def getTheatre(self):
+        return self.__theatre
+
+
+class TheatreB:
+    def __init__(self, theatreName, movieName, showTime, seat, price):
+        self.__theatre = theatreName
+        self.__movieName = movieName
+        self.__showTime = showTime
+        self.__seat = seat
+        self.__price = price
+
+    def setTheatreName(self, Theatre):
+        self.__theatre = Theatre        
+
+    def getTheatreName(self):
+        return self.__theatre    
+
+    def getMovieName(self):            
+        return self.__movieName
+
+    def getShowTime(self):
+        return self.__showTime    
+
+    def setSeat(self, Seat):
+        self.__seat = Seat     
+
+    def getSeat(self):
+        return self.__seat      
+
+    def getPrice(self):
+        return self.__price      
+
+    def getTheatre(self):
+        return self.__theatre    
